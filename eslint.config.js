89f3d934aaca13801a8b874a2f381e5a3fbe25b9
emPeeGee <eempeegee@gmail.com>
2025-01-31 20:05:58 +0200

    ✨ feat: setup prettier & with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {rules: {
-    "react/react-in-jsx-scope": "off",
-  }}
+  eslintPluginPrettierRecommended,
+  { rules: { "react/react-in-jsx-scope": "off" } },
 ];
