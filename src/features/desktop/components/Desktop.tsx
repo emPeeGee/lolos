92d6c2d2968c4586f09e41c0a2efdb9d7a66b9a3
emPeeGee <eempeegee@gmail.com>
2025-02-01 14:52:49 +0200

    ✨ feat: basic desktop with wallpaper

---
+++ src/features/desktop/components/Desktop.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+function Desktop() {
+
+  return (
+    <div className="h-screen w-screen flex flex-col items-center bg-[url(assets/images/wallpaper.png)] bg-cover bg-center">
+      <div className="grid grid-cols-8 gap-4">
+      </div>
+    </div>
+  );
+}
+
+export default Desktop;
--- todo.md
+++ todo.md
@@ -1,1 +1,2 @@
 1. Mouse selection effect
+2. Selected color should be defined somewhere, as like mac os does, not hardcoded, and not so pronounced
