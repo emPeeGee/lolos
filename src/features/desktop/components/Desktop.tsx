6b94fc4df88c3e193a915574976d4f8187b59419
emPeeGee <eempeegee@gmail.com>
2025-02-01 14:53:33 +0200

    ✨ feat: create a desktop icon on desktop screen

--- src/features/desktop/components/Desktop.tsx
+++ src/features/desktop/components/Desktop.tsx
@@ -1,9 +1,34 @@
 import { useState } from 'react';
+import { FolderIcon } from './FolderIcon';
+
+const folderIcons = [
+  { id: 1, name: 'Icon 1' },
+  { id: 2, name: 'Icon 2' },
+  { id: 3, name: 'Icon 3' },
+  { id: 4, name: 'Icon 4' },
+  { id: 5, name: 'Icon 5' },
+  { id: 6, name: 'Icon 6' },
+  { id: 7, name: 'Icon 7' },
+  { id: 8, name: 'Icon 8' },
+];
 function Desktop() {
+  const [selectedIcon, setSelectedIcon] = useState<number | null>(null);
+
+  const handleIconClick = (id: number) => {
+    setSelectedIcon(id);
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col items-center bg-[url(assets/images/wallpaper.png)] bg-cover bg-center">
       <div className="grid grid-cols-8 gap-4">
+        {folderIcons.map((icon) => (
+          <FolderIcon
+            key={icon.id}
+            name={icon.name}
+            isSelected={selectedIcon === icon.id}
+            onClick={() => handleIconClick(icon.id)}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/features/desktop/components/FolderIcon.tsx
@@ -0,0 +1,21 @@
+interface FolderIconProps {
+  name: string;
+  isSelected: boolean;
+  onClick: () => void;
+  onDoubleClick?: () => void;
+}
+
+export function FolderIcon({ name, isSelected, onClick, onDoubleClick }: FolderIconProps) {
+  return (
+    <div className={`flex flex-col items-center `} onClick={onClick} onDoubleClick={onDoubleClick}>
+      <img
+        src="src/assets/images/folder.png"
+        alt={name}
+        className={`h-16 mb-1 bg-cover bg-center ${isSelected ? 'bg-blue-500' : ''}`}
+      />
+      <div className={`w-fit text-center text-white ${isSelected ? 'bg-blue-500' : ''}`}>
+        <span>{name}</span>
+      </div>
+    </div>
+  );
+}
