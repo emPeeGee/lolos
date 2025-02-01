97db16cc27cd5eb88a065374521f86af1878113d
emPeeGee <eempeegee@gmail.com>
2025-02-01 21:39:01 +0200

    🐋 chore: log zustand actions

--- src/features/desktop/store/desktopStore.ts
+++ src/features/desktop/store/desktopStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { Icon } from '@/types';
 
 interface DesktopState {
@@ -11,18 +12,32 @@ interface DesktopState {
   clearSelectedIcons: () => void;
 }
 
-export const useDesktopStore = create<DesktopState>((set) => ({
-  desktopIcons: [
-    { id: 1, name: 'Folder 1', type: 'folder' },
-    { id: 2, name: 'File 1', type: 'file' },
-    { id: 3, name: 'Folder 2', type: 'folder' },
-    { id: 4, name: 'File 2', type: 'file' },
-  ],
-  selectedIcons: [],
-  setDesktopIcons: (icons) => set({ desktopIcons: icons }),
-  setSelectedIcons: (icons) => set({ selectedIcons: icons }),
-  addSelectedIcon: (id) => set((state) => ({ selectedIcons: [...state.selectedIcons, id] })),
-  removeSelectedIcon: (id) =>
-    set((state) => ({ selectedIcons: state.selectedIcons.filter((iconId) => iconId !== id) })),
-  clearSelectedIcons: () => set({ selectedIcons: [] }),
-}));
+export const useDesktopStore = create<DesktopState>()(
+  devtools(
+    (set) => ({
+      desktopIcons: [
+        { id: 1, name: 'Folder 1', type: 'folder' },
+        { id: 2, name: 'File 1', type: 'file' },
+        { id: 3, name: 'Folder 2', type: 'folder' },
+        { id: 4, name: 'File 2', type: 'file' },
+      ],
+      selectedIcons: [],
+      setDesktopIcons: (icons) => set({ desktopIcons: icons }, undefined, 'setDesktopIcons'),
+      setSelectedIcons: (icons) => set({ selectedIcons: icons }, undefined, 'setSelectedIcons'),
+      addSelectedIcon: (id) =>
+        set(
+          (state) => ({ selectedIcons: [...state.selectedIcons, id] }),
+          undefined,
+          'addSelectedIcon',
+        ),
+      removeSelectedIcon: (id) =>
+        set(
+          (state) => ({ selectedIcons: state.selectedIcons.filter((iconId) => iconId !== id) }),
+          undefined,
+          'removeSelectedIcon',
+        ),
+      clearSelectedIcons: () => set({ selectedIcons: [] }, undefined, 'clearSelectedIcons'),
+    }),
+    { name: 'DesktopStore', store: 'DesktopStore' },
+  ),
+);
